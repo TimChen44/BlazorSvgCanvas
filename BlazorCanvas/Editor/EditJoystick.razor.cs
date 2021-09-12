9d9586a8b517ce0b0e5297e8ffd83bf4c95008b2
TimChen <timchen@live.com>
2021-09-12 20:36:21 +0800

    选择提示

--- BlazorCanvas/Editor/EditJoystick.razor.cs
+++ BlazorCanvas/Editor/EditJoystick.razor.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,13 @@ namespace BlazorCanvas
 {
     public partial class EditJoystick
     {
+        [Parameter]
+        public ElementBase Element { get; set; }
+
+        public RectangleF ERect => Element.Rect;
+
+        public double JoyWidth => Math.Ceiling(Element.Rect.Width / 10);
+
+        public double JoyHeight => Math.Ceiling(Element.Rect.Height / 10);
     }
 }
